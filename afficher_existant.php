fbcf93eb5d565f444d55cfe24c9a41e4f654912d
anony tux <camillepire@gmail.com>
2013-04-08 14:16:21 +0200

    update

--- afficher_existant.php
+++ afficher_existant.php
@@ -22,21 +22,21 @@
                         if ($type == 'section') {
                                     $requete = "select * from ".$type;
                                     $reponse = $bdd->query($requete);
-                                    echo"<table id='partage_table'>
+                                    echo"<table id='affiche_table'>
                                         <tr>
-                                            <td id='partage_td_titre'>ID_SECTION</td>
-                                            <td id='partage_td_titre'>NOM_SECTION</td>
-                                            <td id='partage_td_titre'>DIPLOME</td>
-                                            <td id='partage_td_titre'>SPECIALITE</td>
+                                            <td id='affiche_td_titre'>ID_SECTION</td>
+                                            <td id='affiche_td_titre'>NOM_SECTION</td>
+                                            <td id='affiche_td_titre'>DIPLOME</td>
+                                            <td id='affiche_td_titre'>SPECIALITE</td>
                                         </tr>";
 
                                     while($donnees = $reponse -> fetch())
                                             {
                                                     echo"<tr>
-                                                            <td id='partage_td'>".$donnees['ID_SECTION']."</td>
-                                                            <td id='partage_td'>".$donnees['NOM_SECTION']."</td>
-                                                            <td id='partage_td'>".$donnees['DIPLOME']."</td>
-                                                            <td id='partage_td'>".$donnees['SPECIALITER']."</td>
+                                                            <td id='affiche_td'>".$donnees['ID_SECTION']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_SECTION']."</td>
+                                                            <td id='affiche_td'>".$donnees['DIPLOME']."</td>
+                                                            <td id='affiche_td'>".$donnees['SPECIALITER']."</td>
                                                          </tr>";
                                             }
                                     echo"</table>";
@@ -46,20 +46,20 @@
                             
                              $requete = "select * from ".$type;
                                     $reponse = $bdd->query($requete);
-                                    echo"<table id='partage_table'>
+                                    echo"<table id='affiche_table'>
                                         <tr>
-                                            <td id='partage_td_titre'>ID_CLASSE</td>
-                                            <td id='partage_td_titre'>ID_SECTION</td>
-                                            <td id='partage_td_titre'>NOM_CLASSE</td>
+                                            <td id='affiche_td_titre'>ID_CLASSE</td>
+                                            <td id='affiche_td_titre'>ID_SECTION</td>
+                                            <td id='affiche_td_titre'>NOM_CLASSE</td>
                                             
                                         </tr>";
 
                                     while($donnees = $reponse -> fetch())
                                             {
                                                     echo"<tr>
-                                                            <td id='partage_td'>".$donnees['ID_CLASSE']."</td>
-                                                            <td id='partage_td'>".$donnees['ID_SECTION']."</td>
-                                                            <td id='partage_td'>".$donnees['NOM_CLASSE']."</td>
+                                                            <td id='affiche_td'>".$donnees['ID_CLASSE']."</td>
+                                                            <td id='affiche_td'>".$donnees['ID_SECTION']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_CLASSE']."</td>
                                                          </tr>";
                                             }
                                     echo"</table>";
@@ -70,10 +70,10 @@
                             
                              $requete = "select * from ".$type;
                                     $reponse = $bdd->query($requete);
-                                    echo"<table id='partage_table'>
+                                    echo"<table id='affiche_table'>
                                         <tr>
-                                            <td id='partage_td_titre'>ID_MATIERE</td>
-                                            <td id='partage_td_titre'>NOM_MATIERE</td>
+                                            <td id='affiche_td_titre'>ID_MATIERE</td>
+                                            <td id='affiche_td_titre'>NOM_MATIERE</td>
                                             
                                             
                                         </tr>";
@@ -81,13 +81,153 @@
                                     while($donnees = $reponse -> fetch())
                                             {
                                                     echo"<tr>
-                                                            <td id='partage_td'>".$donnees['ID_MATIERE']."</td>
-                                                            <td id='partage_td'>".$donnees['NOM_MATIERE']."</td>
+                                                            <td id='affiche_td'>".$donnees['ID_MATIERE']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_MATIERE']."</td>
                                                          </tr>";
                                             }
                                     echo"</table>";
                                     
                         }
+      // si user   
+                        elseif ($type == 'user') {
+              //administrateur              
+                             $requete = "select * from IPF_USER where NB_LEVEL = 1 ";
+                                    $reponse = $bdd->query($requete);
+                                    echo"<br><h3>Administrateurs</h3><br>
+                                        <table id='affiche_table'>
+                                        <tr>
+                                            <td id='affiche_td_titre'>ID_USER</td>
+                                            <td id='affiche_td_titre'>NB_LEVEL</td>
+                                            <td id='affiche_td_titre'>NOM_USER</td>
+                                            <td id='affiche_td_titre'>PRENOM_USER</td>
+                                            <td id='affiche_td_titre'>MAIL_ECOLE</td>
+                                            <td id='affiche_td_titre'>MOT_DE_PASSE (MDP md5)</td>
+                                            
+                                        </tr>";
+
+                                    while($donnees = $reponse -> fetch())
+                                            {
+                                                    echo"<tr>
+                                                            <td id='affiche_td'>".$donnees['ID_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['NB_LEVEL']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['PRENOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['MAIL_ECOLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['MDP']."</td>
+                                                         </tr>";
+                                            }
+                                    echo"</table><br>";
+                 //eleve                   
+                                $requete = "select * from IPF_USER where NB_LEVEL = 2";
+                                    $reponse = $bdd->query($requete);
+                                    echo"<br><h3>Elèves</h3><br>
+                                        <table id='affiche_table'>
+                                        <tr>
+                                            <td id='affiche_td_titre'>ID_USER</td>
+                                            <td id='affiche_td_titre'>NB_LEVEL</td>
+                                            <td id='affiche_td_titre'>NOM_USER</td>
+                                            <td id='affiche_td_titre'>PRENOM_USER</td>
+                                            <td id='affiche_td_titre'>MAIL_ECOLE</td>
+                                            <td id='affiche_td_titre'>MOT_DE_PASSE (MDP md5)</td>
+                                            
+                                        </tr>";
+
+                                    while($donnees = $reponse -> fetch())
+                                            {
+                                                    echo"<tr>
+                                                            <td id='affiche_td'>".$donnees['ID_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['NB_LEVEL']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['PRENOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['MAIL_ECOLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['MDP']."</td>
+                                                         </tr>";
+                                            }
+                                    echo"</table><br>"; 
+                   //proffesseur
+                                    $requete = "select * from IPF_USER where NB_LEVEL = 3";
+                                    $reponse = $bdd->query($requete);
+                                    echo"<br><h3>Professeurs</h3><br>
+                                        <table id='affiche_table'>
+                                        <tr>
+                                            <td id='affiche_td_titre'>ID_USER</td>
+                                            <td id='affiche_td_titre'>NB_LEVEL</td>
+                                            <td id='affiche_td_titre'>NOM_USER</td>
+                                            <td id='affiche_td_titre'>PRENOM_USER</td>
+                                            <td id='affiche_td_titre'>MAIL_ECOLE</td>
+                                            <td id='affiche_td_titre'>MOT_DE_PASSE (MDP md5)</td>
+                                            
+                                        </tr>";
+
+                                    while($donnees = $reponse -> fetch())
+                                            {
+                                                    echo"<tr>
+                                                            <td id='affiche_td'>".$donnees['ID_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['NB_LEVEL']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['PRENOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['MAIL_ECOLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['MDP']."</td>
+                                                         </tr>";
+                                            }
+                                    echo"</table><br>";
+                     //administration
+                                    $requete = "select * from IPF_USER where NB_LEVEL = 4";
+                                    $reponse = $bdd->query($requete);
+                                    echo"<br><h3>Administration</h3><br>
+                                        <table id='affiche_table'>
+                                        <tr>
+                                            <td id='affiche_td_titre'>ID_USER</td>
+                                            <td id='affiche_td_titre'>NB_LEVEL</td>
+                                            <td id='affiche_td_titre'>NOM_USER</td>
+                                            <td id='affiche_td_titre'>PRENOM_USER</td>
+                                            <td id='affiche_td_titre'>MAIL_ECOLE</td>
+                                            <td id='affiche_td_titre'>MOT_DE_PASSE (MDP md5)</td>
+                                            
+                                        </tr>";
+
+                                    while($donnees = $reponse -> fetch())
+                                            {
+                                                    echo"<tr>
+                                                            <td id='affiche_td'>".$donnees['ID_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['NB_LEVEL']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['PRENOM_USER']."</td>
+                                                            <td id='affiche_td'>".$donnees['MAIL_ECOLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['MDP']."</td>
+                                                         </tr>";
+                                            }
+                                    echo"</table><br>";
+                                    
+                        }// fin user  
+                        
+            // si salle    
+                        elseif ($type == 'salle') {
+                            
+                             $requete = "select * from ".$type;
+                                    $reponse = $bdd->query($requete);
+                                    echo"<table id='affiche_table'>
+                                        <tr>
+                                            <td id='affiche_td_titre'>ID_SALLE</td>
+                                            <td id='affiche_td_titre'>NOM_SALLE</td>
+                                            <td id='affiche_td_titre'>BATIMENT</td>
+                                            <td id='affiche_td_titre'>NUM_SALLE</td>
+                                            
+                                            
+                                        </tr>";
+
+                                    while($donnees = $reponse -> fetch())
+                                            {
+                                                    echo"<tr>
+                                                            <td id='affiche_td'>".$donnees['ID_SALLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['NOM_SALLE']."</td>
+                                                            <td id='affiche_td'>".$donnees['BATIMENT']."</td>
+                                                            <td id='affiche_td'>".$donnees['NUM_SALLE']."</td>    
+                                                         </tr>";
+                                            }
+                                    echo"</table>";
+                                    
+                        }            
                         
                         
                    

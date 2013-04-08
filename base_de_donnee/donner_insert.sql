d00a1aad6cb8b7d63da1c3795a5538b6405a97c1
anony tux <camillepire@gmail.com>
2013-04-08 19:16:17 +0200

    page admin fini insert a jour

--- base_de_donnee/donner_insert.sql
+++ base_de_donnee/donner_insert.sql
@@ -6,11 +6,7 @@
 # #######################################################################
 #
 
-									
-insert into SALLE values(1,'porte champéret',2.6,'salle chouaki'),
-			(2,'porte champéret',2.7,'salle velon'),
-			(3,'porte champéret',2.8,'salle vinet'),
-			(4,'porte champéret',1.6,'salle mus');
+
 						
 insert into level values(1,1,1),
                         (2,0,1),
@@ -45,12 +41,4 @@ Create user generique@'localhost' identified by 'iris';
 grant select on ipf_com.IPF_USER to generique@'localhost';
 grant select on ipf_com.eleve to generique@'localhost';
 grant select on ipf_com.classe to generique@'localhost';
-
-
-insert into enseigne(ID_CLASSE,ID_MATIERE,ID_SALLE,ID_USER,DATE_DEBUT,DATE_FIN)
-                          values(1,2,3,8,20130226110000,20130226120000),
-                                (1,4,4,7,20130180090000,20130218103000),
-                                (1,4,4,6,20130227170000,20130227183000),
-                                (2,4,4,7,20130180090000,20130218103000),
-                                (2,4,4,6,20130227170000,20130227183000);
 
